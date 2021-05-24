cdd905f7c309edc5b42cb94a275c72ca5a4b55cb
Ferguson666 <56108929+Ferguson666@users.noreply.github.com>
2021-05-24 23:59:58 +0800

    ZSL
    毕设相关代码

---
+++ util.py
@@ -0,0 +1,173 @@
+import numpy as np
+import scipy.io as sio
+import torch
+from sklearn import preprocessing
+import model
+from sklearn.cluster import KMeans
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+        m.bias.data.fill_(0)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+def map_label(label, classes):
+    mapped_label = torch.LongTensor(label.size())
+    for i in range(classes.size(0)):
+        mapped_label[label == classes[i]] = i
+
+    return mapped_label
+
+
+class Logger(object):
+    def __init__(self, filename):
+        self.filename = filename
+        f = open(self.filename + '.log', "a")
+        f.close()
+
+    def write(self, message):
+        f = open(self.filename + '.log', "a")
+        f.write(message)
+        f.close()
+
+
+class DATA_LOADER(object):
+    def __init__(self, opt):
+        if opt.matdataset:
+            self.read_matdataset(opt)
+        self.index_in_epoch = 0
+        self.epochs_completed = 0
+
+        self.feature_dim = self.train_feature.shape[1]
+        self.att_dim = self.attribute.shape[1]
+        self.text_dim = self.att_dim
+        self.train_cls_num = self.seenclasses.shape[0]
+        self.test_cls_num = self.unseenclasses.shape[0]
+        self.tr_cls_centroid = np.zeros([self.seenclasses.shape[0], self.feature_dim], np.float32)
+        for i in range(self.seenclasses.shape[0]):
+            self.tr_cls_centroid[i] = np.mean(
+                self.train_feature[torch.nonzero(self.train_mapped_label == i), :].numpy(), axis=0)
+
+        n_cluster = opt.n_clusters
+        real_proto = torch.zeros(n_cluster * self.train_cls_num, self.feature_dim)
+        for i in range(self.train_cls_num):
+            sample_idx = (self.train_mapped_label == i).nonzero().squeeze()
+            if sample_idx.numel() == 0:
+                real_proto[n_cluster * i: n_cluster * (i + 1)] = torch.zeros(n_cluster, self.feature_dim)
+            else:
+                real_sample_cls = self.train_feature[sample_idx, :]
+                y_pred = KMeans(n_clusters=n_cluster, random_state=3).fit_predict(real_sample_cls)
+                for j in range(n_cluster):
+                    real_proto[n_cluster * i + j] = torch.from_numpy(
+                        real_sample_cls[torch.nonzero(torch.from_numpy(y_pred) == j), :].mean(dim=0).cpu().numpy())
+        self.real_proto = real_proto
+
+    def read_matdataset(self, opt):
+        matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.image_embedding + ".mat")
+        feature = matcontent['features']
+        label = matcontent['lable'].T.astype(int).squeeze() - 1
+        matcontent = sio.loadmat(opt.dataroot + "/" + opt.dataset + "/" + opt.class_embedding + ".mat")
+        # numpy array index starts from 0, matlab starts from 1
+        trainval_loc = matcontent['trainval_loc'].squeeze() - 1
+        train_loc = matcontent['train_loc'].squeeze() - 1
+        val_unseen_loc = matcontent['val_loc'].squeeze() - 1
+        test_seen_loc = matcontent['test_seen_loc'].squeeze() - 1
+        test_unseen_loc = matcontent['test_unseen_loc'].squeeze() - 1
+        self.attribute = torch.from_numpy(matcontent['att'].T).float()
+
+        if not opt.validation:
+            if opt.preprocessing:
+                if opt.standardization:
+                    print('standardization...')
+                    scaler = preprocessing.StandardScaler()
+                else:
+                    scaler = preprocessing.MinMaxScaler()
+
+                _train_feature = scaler.fit_transform(feature[trainval_loc])
+                _test_seen_feature = scaler.transform(feature[test_seen_loc])
+                _test_unseen_feature = scaler.transform(feature[test_unseen_loc])
+                self.train_feature = torch.from_numpy(_train_feature).float()
+                mx = self.train_feature.max()
+                self.train_feature.mul_(1 / mx)
+                self.train_label = torch.from_numpy(label[trainval_loc]).long()
+                self.test_unseen_feature = torch.from_numpy(_test_unseen_feature).float()
+                self.test_unseen_feature.mul_(1 / mx)
+                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long()
+                self.test_seen_feature = torch.from_numpy(_test_seen_feature).float()
+                self.test_seen_feature.mul_(1 / mx)
+                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
+            else:
+                self.train_feature = torch.from_numpy(feature[trainval_loc]).float()
+                self.train_label = torch.from_numpy(label[trainval_loc]).long()
+                self.test_unseen_feature = torch.from_numpy(feature[test_unseen_loc]).float()
+                self.test_unseen_label = torch.from_numpy(label[test_unseen_loc]).long()
+                self.test_seen_feature = torch.from_numpy(feature[test_seen_loc]).float()
+                self.test_seen_label = torch.from_numpy(label[test_seen_loc]).long()
+        else:
+            self.train_feature = torch.from_numpy(feature[train_loc]).float()
+            netA = model.ChannelAttention(self)
+            self.train_feature = netA( self.self.train_feature.reshape(1213,2,2,512))
+            self.train_feature = self.self.train_feature.reshape(1213,2048)
+            self.train_label = torch.from_numpy(label[train_loc]).long()
+            self.test_unseen_feature = torch.from_numpy(feature[val_unseen_loc]).float()
+            self.test_unseen_label = torch.from_numpy(label[val_unseen_loc]).long()
+
+        self.seenclasses = torch.from_numpy(np.unique(self.train_label.numpy()))
+        self.unseenclasses = torch.from_numpy(np.unique(self.test_unseen_label.numpy()))
+        self.ntrain = self.train_feature.size()[0]
+        self.ntrain_class = self.seenclasses.size(0)
+        self.ntest_class = self.unseenclasses.size(0)
+        self.train_class = self.seenclasses.clone()
+        self.allclasses = torch.arange(0, self.ntrain_class + self.ntest_class).long()
+        self.train_mapped_label = map_label(self.train_label, self.seenclasses)
+        self.train_att = self.attribute[self.seenclasses].numpy()
+        self.test_att = self.attribute[self.unseenclasses].numpy()
+        self.train_cls_num = self.ntrain_class
+        self.test_cls_num = self.ntest_class
+
+    def next_batch_one_class(self, batch_size):
+        if self.index_in_epoch == self.ntrain_class:
+            self.index_in_epoch = 0
+            perm = torch.randperm(self.ntrain_class)
+            self.train_class[perm] = self.train_class[perm]
+
+        iclass = self.train_class[self.index_in_epoch]
+        idx = self.train_label.eq(iclass).nonzero().squeeze()
+        perm = torch.randperm(idx.size(0))
+        idx = idx[perm]
+        iclass_feature = self.train_feature[idx]
+        iclass_label = self.train_label[idx]
+        self.index_in_epoch += 1
+        return iclass_feature[0:batch_size], iclass_label[0:batch_size], self.attribute[iclass_label[0:batch_size]]
+
+    def next_batch(self, batch_size):
+        idx = torch.randperm(self.ntrain)[0:batch_size]
+        batch_feature = self.train_feature[idx]
+        batch_label = self.train_label[idx]
+        batch_att = self.attribute[batch_label]
+        return batch_feature, batch_label, batch_att
+
+    # select batch samples by randomly drawing batch_size classes
+    def next_batch_uniform_class(self, batch_size):
+        batch_class = torch.LongTensor(batch_size)
+        for i in range(batch_size):
+            idx = torch.randperm(self.ntrain_class)[0]
+            batch_class[i] = self.train_class[idx]
+
+        batch_feature = torch.FloatTensor(batch_size, self.train_feature.size(1))
+        batch_label = torch.LongTensor(batch_size)
+        batch_att = torch.FloatTensor(batch_size, self.attribute.size(1))
+        for i in range(batch_size):
+            iclass = batch_class[i]
+            idx_iclass = self.train_label.eq(iclass).nonzero().squeeze()
+            idx_in_iclass = torch.randperm(idx_iclass.size(0))[0]
+            idx_file = idx_iclass[idx_in_iclass]
+            batch_feature[i] = self.train_feature[idx_file]
+            batch_label[i] = self.train_label[idx_file]
+            batch_att[i] = self.attribute[batch_label[i]]
+        return batch_feature, batch_label, batch_att
